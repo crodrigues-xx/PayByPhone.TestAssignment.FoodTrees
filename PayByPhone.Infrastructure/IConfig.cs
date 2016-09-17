4c144d97da086ac80f33528a3b5528851a87cbdd
crodrigues@l3ls.com <crodrigues@l3ls.com>
2016-09-16 20:13:00 -0700

    .NET 5.2 satellite projects

---
+++ PayByPhone.Infrastructure/IConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayByPhone.Infrastructure
+{
+    public interface IConfig
+    {
+        string Retrieve(string key);
+    }
+}
---
+++ PayByPhone.TestAssignment.FoodTrees.Services/FoodSearchAmazonDynamo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PayByPhone.TestAssignment.FoodTrees.Models;
+
+namespace PayByPhone.TestAssignment.FoodTrees.Services
+{
+    public class FoodSearchAmazonDynamo : IFoodSearch
+    {
+        public IList<FoodGarden> GetFoodGardensByFoodTreeVarieties(string foodTreeVariety, 
+                                                                   string sortHoistedFoodTreeVariety, 
+                                                                   IList<string> excludedJurisdictions, 
+                                                                   IList<string> excludedMapIds)
+        {
+            IList<FoodGarden> foodGardens = null;
+
+            return foodGardens;
+        }
+    }
+}
