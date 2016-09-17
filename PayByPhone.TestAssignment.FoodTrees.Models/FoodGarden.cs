a64936f474fb2a72f23c1cad282e36b3983af6a5
crodrigues@l3ls.com <crodrigues@l3ls.com>
2016-09-16 18:40:53 -0700

    models and services projects

---
+++ PayByPhone.TestAssignment.FoodTrees.Models/FoodGarden.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayByPhone.TestAssignment.FoodTrees.Models
+{
+    public class FoodGarden
+    {
+        public string MapId { get; set; }
+        public int YearCreated { get; set; }
+        public string Name { get; set; }
+        public string MergedAddress { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public int NumberOfPlots { get; set; }
+        public int NumberOfFoodTrees { get; set; }
+        public string Notes { get; set; }
+        public string FoodTreeVarieties { get; set; }
+        public string Jurisdiction { get; set; }
+    }
+}
---
+++ PayByPhone.TestAssignment.FoodTrees.Models/IFoodSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayByPhone.TestAssignment.FoodTrees.Models
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public interface IFoodSearch
+    {
+        IList<FoodGarden> GetFoodGardensByFoodTreeVarieties(string foodTreeVariety,
+                                                            string sortHoistedFoodTreeVariety,
+                                                            IList<string> excludedJurisdictions,
+                                                            IList<string> excludedMapIds);
+
+    }
+}
